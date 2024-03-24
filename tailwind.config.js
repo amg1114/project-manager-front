fe2272a1a6034dc13964d33419e9aae27f1a80e7
Alejandro Moreno <alejandromogil@gmail.com>
2024-03-24 15:03:58 -0500

    Login, Register Forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,ts}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Raleway", "sans-serif"],
+            },
+            colors: {
+              "blue-light-1":"#81C3D7",
+              "blue-light-2": "#3A7CA5",
+              "blue-neutral": "#2F6690",
+              "blue-dark": "#16425B",
+              "beige-1": "#ebede9",
+            }
+        },
+    },
+    plugins: [],
+};
