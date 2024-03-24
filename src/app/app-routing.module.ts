f72caee35629c4600c25e5a965b8602b9d1caf5f
Alejandro Moreno <alejandromogil@gmail.com>
2024-03-24 14:21:31 -0500

    Auth Components Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'login',
+        loadChildren: () =>
+            import('./auth/auth.module').then((m) => m.AuthModule),
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login.component';
+import { RegisterComponent } from './pages/register.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent,
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class AuthRoutingModule {}
