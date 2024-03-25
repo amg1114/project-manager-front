464142ff1189aa0236f73006a5c7096f16784d00
Alejandro Moreno <alejandromogil@gmail.com>
2024-03-25 10:11:59 -0500

    Get Current Workflow on Workflow Detail

--- src/app/dashboard/pages/workflow/workflow.component.ts
+++ src/app/dashboard/pages/workflow/workflow.component.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WorkflowI } from '../../interfaces/workflow.interface';
+import { Router, ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { AuthService } from '../../../auth/services/auth.service';
+import { UsersService } from '../../services/users.service';
+import { WorkflowsService } from '../../services/workflows.service';
 
 @Component({
-  selector: 'dashboard-workflow',
-  templateUrl: './workflow.component.html'
+    selector: 'dashboard-workflow',
+    templateUrl: './workflow.component.html',
 })
-export class WorkflowComponent {
+export class WorkflowComponent implements OnInit {
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private authService: AuthService,
+        private workflowsService: WorkflowsService,
+        private usersService: UsersService
+    ) {}
 
+    public workflow?: WorkflowI;
+
+    ngOnInit(): void {
+      this.getCurrentWorkflow()
+    }
+
+    getCurrentWorkflow() {
+        const loggedUser = this.authService.getLoggedUser();
+        this.activatedRoute.params
+            .pipe(
+                switchMap(({ slug }) =>
+                    this.workflowsService.getWorkflowBySlug(
+                        slug,
+                        loggedUser!.sub
+                    )
+                )
+            )
+            .subscribe((workflow) => {
+                if (!workflow) return this.router.navigateByUrl('');
+                console.log(workflow);
+                
+                return (this.workflow = workflow);
+            });
+    }
 }
